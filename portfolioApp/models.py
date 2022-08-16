fc8e43aab360da08c81e6b6b6bd84be725757b85
pururajatgithub <pururaj2000@gmail.com>
2022-08-16 18:11:21 +0530

    modified dockerfile

--- portfolioApp/models.py
+++ portfolioApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class category(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/', blank=True)
+
+    def __str__(self):
+        return self.name
